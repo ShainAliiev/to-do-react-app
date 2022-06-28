efb26fa22f75b7302e3f54f591f1de983897c106
ShainAliiev <shain.aliiev@gmail.com>
2022-06-28 16:06:53 -0400

    New1

--- src/data/todosData.js
+++ src/data/todosData.js
@@ -1,40 +1,40 @@
-const todos = "ExpenseItems"
 
 
 
-// const todos = [
-//   {
-//     "name": "Tiger Nixon",
-//     "position": "System Architect",
-//     "office": "Edinburgh",
-//     "extn.": "5421",
-//     "start date": "2011/04/25",
-//     "salary": "$320,800"
-//   },
-//   {
-//     "name": "Garrett Winters",
-//     "position": "Accountant",
-//     "office": "Tokyo",
-//     "extn.": "8422",
-//     "start date": "2011/07/25",
-//     "salary": "$170,750"
-//   },
-//   {
-//     "name": "Ashton Cox",
-//     "position": "Junior Technical Author",
-//     "office": "San Francisco",
-//     "extn.": "1562",
-//     "start date": "2009/01/12",
-//     "salary": "$86,000"
-//   },
-//   {
-//     "name": "Cedric Kelly",
-//     "position": "Senior Javascript Developer",
-//     "office": "Edinburgh",
-//     "extn.": "6224",
-//     "start date": "2012/03/29",
-//     "salary": "$433,060"
-//   }
-// ]
+
+const todos = [
+  {
+    "name": "Tiger Nixon",
+    "position": "System Architect",
+    "office": "Edinburgh",
+    "extn.": "5421",
+    "start date": "2011/04/25",
+    "salary": "$320,800"
+  },
+  {
+    "name": "Garrett Winters",
+    "position": "Accountant",
+    "office": "Tokyo",
+    "extn.": "8422",
+    "start date": "2011/07/25",
+    "salary": "$170,750"
+  },
+  {
+    "name": "Ashton Cox",
+    "position": "Junior Technical Author",
+    "office": "San Francisco",
+    "extn.": "1562",
+    "start date": "2009/01/12",
+    "salary": "$86,000"
+  },
+  {
+    "name": "Cedric Kelly",
+    "position": "Senior Javascript Developer",
+    "office": "Edinburgh",
+    "extn.": "6224",
+    "start date": "2012/03/29",
+    "salary": "$433,060"
+  }
+]
 
 export default todos
